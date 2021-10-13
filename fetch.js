c5cd5a8ea0af4d58477d77508a80f33df79a1b54
Jesus alberto Rojas gamboa <jesusrojas@MacBook-Pro-de-Jesus.local>
2021-10-13 11:49:35 +0200

    pokeappi

--- fetch.js
+++ fetch.js
@@ -2,30 +2,18 @@ const ul=document.querySelector('.display');
 
 
 window.addEventListener("scroll", () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-})
-//     const{ scrollHeight , scrollTop, clientHeight} = document.documentElement;
-//     if(scrollTop + clientHeight < scrollHeight - 100);
-// });
-//  function progreso (){
-//      let scroll = documentElement.scrollTop
-//      let alto = document.documentElement.scrollHeight - documentElement.clientHeight
-//      let progreso = (scroll/alto)*100;
-//      document.getElementsByClassName("barra")[0].style.width = progreso+"%";
-//  }
-// let ubicacionPrincipal = window.pageXOffset;
-// function menu(){
-//     let desplazamientoActual = window.pageXOffset;
-//     if(ubicacionPrincipal >= desplazamientoActual){
-// document.getElementsByClassName("display").style.top = "0";
-//     }else{
-//         document.getElementsByClassName("display").style.top = "-100";
-//     }
-// }
+  if (scrollTop + clientHeight + 0.5 >= scrollHeight) {
+    lastPokemon += 10;
+    getPokemons();
+  }
+});
+
 
 const getPokemons= async ()=>{
 let pokemonJsonOutSide =[];    
-for (let i = 1; i < 152; i++) {
+for (let i = 1; i < lastPokemon && i < 152; i++) {
     let pokemonUrl = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     let pokemonsJson = await pokemonUrl.json();
     pokemonJsonOutSide.push(pokemonsJson)
@@ -37,7 +25,7 @@ const mappedPokemon= pokemonJsonOutSide.map((Element) => ({
     id: Element.id,
     image1: Element.sprites.other["official-artwork"]["front_default"],
     image2: Element.sprites.other["dream_world"]["front_default"],
-    types: Element.types[0].type.name
+    
    
 }));
 
@@ -48,7 +36,6 @@ const displayPokemon=(mappedPokemon)=>{
     const pokemonsHtml=mappedPokemon.map(
         (element)=>
         `<li><h3>${element.name}</h3>
-        <p>${Element.types}</p>
         <img src="${element.image1}" alt="${element.name}"/>
         <img src="${element.image2}" alt="${element.name}"/>
         
